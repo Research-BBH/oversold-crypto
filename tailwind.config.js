c981b769f841d1aae0b7b0a53672b81a398fe465
Research-BBH <research@bigbrain.holdings>
2026-01-28 21:00:41 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ export default {
   plugins: [],
   safelist: [
     { pattern: /(bg|text|border)-(red|orange|yellow|green|emerald|gray)-(400|500|600)/ },
-    { pattern: /(bg|border)-(red|orange|yellow|green|emerald|gray)-500\/(10|20|30|40)/ },
+    { pattern: /(bg|border)-(red|orange|yellow|green|emerald|gray)-500\/(10|20|30|40|50)/ },
+    { pattern: /shadow-(red|orange|yellow|green|emerald|gray)-500\/(10|20|30)/ },
+    'border-2',
   ],
 }
